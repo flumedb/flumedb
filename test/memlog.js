504df037d98d1b6a438247d6d7f5b3c9f5b7d040
Dominic Tarr <dominic.tarr@gmail.com>
2016-11-04 15:55:18 +0000

    reusable tests

--- test/memlog.js
+++ test/memlog.js
@@ -7,40 +7,48 @@ var Flume = require('../')
 var MemLog = require('flumelog-memory')
 var Reduce = require('flumeview-reduce')
 
-var db =
-  Flume(MemLog())
-    .use('stats', Reduce(function (acc, data) {
-      return statistics(acc, data.foo)
-    }))
+module.exports = function (db) {
+  db.use('stats', Reduce(function (acc, data) {
+    return statistics(acc, data.foo)
+  }))
 
+  tape('simple', function (t) {
 
-tape('simple', function (t) {
+    db.since(function (v) { 
+      console.log("SINCE", v)
+    }, false)
 
-  db.append({foo: 1}, function (err, seq) {
-    if(err) throw err
-    console.log("GET", err, seq)
-    db.stats.get([], function (err, value) {
+    db.append({foo: 1}, function (err, seq) {
       if(err) throw err
-      console.log(  "GET", value)
-      t.deepEqual(value.mean, 1)
-      t.deepEqual(value.stdev, 0)
-      t.end()
-    })
-  })
-})
-
-tape('append', function (t) {
-  db.append({foo: 3}, function (err, seq) {
-    if(err) throw err
-    console.log("GET", err, seq)
-    db.stats.get([], function (err, value) {
-      if(err) throw err
-      console.log(  "GET", value)
-      t.deepEqual(value.mean, 2)
-      t.deepEqual(value.stdev, 1)
-      t.end()
+      console.log("GET", err, seq, db.stats)
+      db.stats.get([], function (err, value) {
+        if(err) throw err
+        console.log(  "GET", value)
+        t.deepEqual(value.mean, 1)
+        t.deepEqual(value.stdev, 0)
+        t.end()
+      })
     })
   })
 
+  tape('append', function (t) {
+    db.append({foo: 3}, function (err, seq) {
+      if(err) throw err
+      console.log("GET", err, seq)
+      db.stats.get([], function (err, value) {
+        if(err) throw err
+        console.log(  "GET", value)
+        t.deepEqual(value.mean, 2)
+        t.deepEqual(value.stdev, 1)
+        t.end()
+      })
+    })
+  })
 
-})
+}
+
+if(!module.parent)
+  module.exports(Flume(MemLog()))
+
+
+
