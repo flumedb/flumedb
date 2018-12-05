bf72c69f7b094cb67a0517b2ae28e93d682bc506
Dominic Tarr <dominic.tarr@gmail.com>
2018-12-06 09:29:47 +1300

    comment about rebuilding too-far ahead log

--- index.js
+++ index.js
@@ -122,6 +122,7 @@ module.exports = function (log, isReady, mapper) {
 
       views[name] = flume[name] = wrap(sv, flume)
       meta[name] = flume[name].meta
+      //if the view is ahead of the log somehow, destroy the view and rebuild it.
       sv.since.once(function build (upto) {
         log.since.once(function (since) {
           if(upto > since) {
